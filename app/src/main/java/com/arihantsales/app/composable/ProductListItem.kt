b9312a332b6e10ae24c049206d9b0f96ab198d8e
Bhavesh Kumawat <kumawatbhavesh1000@gmail.com>
2023-08-16 21:40:28 +0530

    changed product list ui

--- app/src/main/java/com/arihantsales/app/composable/ProductListItem.kt
+++ app/src/main/java/com/arihantsales/app/composable/ProductListItem.kt
@@ -1,12 +1,10 @@
 package com.arihantsales.app.composable
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,7 +13,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -26,39 +23,40 @@ import com.arihantsales.app.domain.model.Product
 
 @Composable
 fun ProductListItem(product: Product, onItemClick: (Product) -> Unit){
-    Box(modifier = Modifier
+    Card(modifier = Modifier
+        .width(150.dp)
+        .padding(5.dp)
         .clickable {
             onItemClick(product)
-        }
-        .background(color = MaterialTheme.colorScheme.inversePrimary)) {
-        Column(modifier = Modifier
-            .padding(10.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally){
-            Card(modifier = Modifier.height(200.dp)
-                .width(150.dp)
-                .padding(0.dp),
-                shape = RoundedCornerShape(15.dp),
-                elevation = CardDefaults.cardElevation(5.dp),
-                border = BorderStroke(2.dp, Color.Black)
+        },
+        shape = RoundedCornerShape(5.dp),
+        elevation = CardDefaults.cardElevation(2.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.onPrimary)
+//        border = BorderStroke(1.dp, Color.Black)
+    ) {
+            Box(modifier = Modifier.aspectRatio(1f).fillMaxWidth()
+                .background(color = Color.Gray)
             )
-            {
-
-            }
-            Text(text = product.name, textAlign = TextAlign.Center, fontWeight = FontWeight.Bold)
-            Text(text = "100$")
-        }
+            Text(
+                text = product.name,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth()
+                ,
+                textAlign = TextAlign.Center
+            )
+            Text(text = "price 100$", modifier = Modifier.fillMaxWidth()
+                , textAlign = TextAlign.Center)
     }
 
 }
 
 @Preview(showBackground = true)
 @Composable
-fun itemPreiew(){
+fun ItemPreiew(){
     ProductListItem(
         Product(
             "jkhkjdfakjdf",
-            "Product Name",
+            "Product name",
             "example.jpg",
             "description"
     )
--- app/src/main/java/com/arihantsales/app/presentation/screens/navigation_screens/HomeScreen.kt
+++ app/src/main/java/com/arihantsales/app/presentation/screens/navigation_screens/HomeScreen.kt
@@ -3,11 +3,12 @@ package com.arihantsales.app.presentation.screens.navigation_screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -23,9 +24,9 @@ fun HomeScreen(
     val state = viewModel.state.value
     val productList = state.products
     LazyVerticalStaggeredGrid(columns = StaggeredGridCells.Adaptive(150.dp),
-        contentPadding = PaddingValues(5.dp),
-        verticalItemSpacing = 7.dp,
-        horizontalArrangement = Arrangement.spacedBy(7.dp)
+        modifier = Modifier.padding(start = 5.dp, end = 5.dp),
+        verticalItemSpacing = 5.dp,
+        horizontalArrangement = Arrangement.spacedBy(5.dp)
         ){
         items(productList){product->
             ProductListItem(product){
