4982a6a8cfb45b3e7f8e4202f24246a2406adf97
Bhavesh Kumawat <kumawatbhavesh1000@gmail.com>
2023-08-28 16:52:55 +0530

    added new Routes

--- app/src/main/java/com/arihantsales/app/presentation/navigation/Routes.kt
+++ app/src/main/java/com/arihantsales/app/presentation/navigation/Routes.kt
@@ -4,4 +4,6 @@ sealed class Routes(val route: String) {
     object Login : Routes("login")
     object MainContent : Routes("mainContent")
     object SignUp : Routes("signup")
+    object ProductList : Routes("product_list_screen")
+    object ProductDetail : Routes("product_detail_screen")
 }
