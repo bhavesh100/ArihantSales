dd4cff74deb32725c0ec89580ce5cd0c90f96509
Bhavesh Kumawat <kumawatbhavesh1000@gmail.com>
2023-09-03 16:40:09 +0530

    added an object in BottomBarScreen.kt which is Cart

--- app/src/main/java/com/arihantsales/app/presentation/navigation/BottomBarScreen.kt
+++ app/src/main/java/com/arihantsales/app/presentation/navigation/BottomBarScreen.kt
@@ -4,10 +4,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class BottomBarScreen(val route : String,val title:String,val icon: ImageVector) {
     object Home : BottomBarScreen("home", "Home", Icons.Default.Home)
     object Favorite : BottomBarScreen("favorite", "Favourite", Icons.Default.Favorite)
     object Profile : BottomBarScreen("profile", "Profile", Icons.Default.Person)
+    object Cart : BottomBarScreen("cart", "Cart", Icons.Default.ShoppingCart)
 }
