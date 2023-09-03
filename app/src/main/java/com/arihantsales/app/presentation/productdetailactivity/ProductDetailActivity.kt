838c9a9e43bfaf74c4db8c85d8a4de99e8e42d16
Bhavesh Kumawat <kumawatbhavesh1000@gmail.com>
2023-09-03 16:42:03 +0530

    made MainScreen.kt clean

--- app/src/main/java/com/arihantsales/app/presentation/productdetailactivity/ProductDetailActivity.kt
+++ app/src/main/java/com/arihantsales/app/presentation/productdetailactivity/ProductDetailActivity.kt
@@ -5,9 +5,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.arihantsales.app.common.composable.TopBar
@@ -26,9 +33,33 @@ class ProductDetailActivity: ComponentActivity() {
             val description = intent.getStringExtra(Routes.ProductDetail.route+"/description")
             ArihantSalesTheme {
                 Scaffold(
-                    topBar = {
-                        TopBar("Arihant sales")
-                    }, content = {
+                    topBar = { TopBar("Arihant sales") },
+                    bottomBar = { Row(
+                        modifier = Modifier
+                            .padding(10.dp)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                    .fillMaxWidth(0.4f),
+                            contentAlignment = Alignment.Center
+                        ) { Text(
+                            text = "100$",
+                            style = MaterialTheme.typography.headlineMedium
+                        ) }
+                        Box(
+                            modifier = Modifier
+                        ) { Button(
+                            onClick = {
+
+                            },
+                            modifier = Modifier.fillMaxWidth(),
+                            shape = RoundedCornerShape(15.dp)
+                        ){
+                            Text(text = "Buy Now")
+                        } }
+
+                    } },
+                    content = {
                         Box(
                             modifier = Modifier
                                 .padding(
